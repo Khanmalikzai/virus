0ef7c9b097deb15ebe6452620e68e6095277c8fd
Khanmalikzai <108901689+Khanmalikzai@users.noreply.github.com>
2022-07-24 17:54:49 +0500

    Create virus.py
    pkg update 
    pkg upgrade
    pkg install git
    pkg install python
    pkg install python2
    pip2 install requests
    pip2 install mechanize
    git clone https://github.com/Khanmalikzai/virus.git
    ls
    cd virus
    ls
    python virus.py

---
+++ virus.py
@@ -0,0 +1,216 @@
+print ("Welcome...!")
+
+
+
+
+
+
+
+virous = ("▁ ▂ ▄ ▅ ▆ ▇ █ [ ｱ尺oｷｷ乇丂o尺乇ﾚんﾑズ乇尺💣🎃👽❌💥🌈⚡🔥 ] █ ▇ ▆ ▅ ▄ ▂ ▁")
+print ("Virous1....")
+
+
+
+
+
+
+
+
+print (virous *10000)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+virousa = ("🇫 🇺 🇨 🇰 🇾 🇴 🇺 💣🔥⚡🥀💥💥🌈❌👽🎃🈲🈂🉑💣🈴❗㊙🈳🈸🈶🈯㊗👾🈂")
+
+print ("Virous2.....")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+print (virousa *10000)
+
+
+
+
+
+
+
+
+
+
+
+
+
+virousw = ("¸„.-•~¹°”^~¨ [ V♥E♥N♥O♥M♥ ] ¨~^”°¹~•-.„¸")
+print ("Virous3....")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+print (virousw *10000)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+viroust = (".o0×ﾒ×0o. [ ﾌoズ乇尺ﾑ乃刀ﾚo丂丂乇ｷ乇尺 ] .o0×ﾒ×0o.")
+print ("Virous4....")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+print (viroust *10000)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+viroup = ("▌│█║▌║▌║ [ IПƧΉƬΛIП ] ║▌║▌║█│▌")
+
+
+
+
+
+
+
+
+print ("Virous5.....")
+
+
+
+
+
+
+
+
+
+
+
+print (viroup *10000)
+
+print ("Virous6....!")
+
+
+gaber = ("Շץρﻉ รѻ๓ﻉՇɦٱกﻭ👾🈸🈳🈯🈶🖤💣🈴❗❌👽🎃🈲🉑⚡🔥🉐💥🌈❌💣")
+
+print (gaber *10000)
+
+print ("Virous7....!")
+
+
+gaberr = ("♕g♕a♕b♕e♕r♕f♕a♕h♕m♕y♕👑💣⚡🌈🔥🎃🔞💯❗❌✅👻☠️")
+
+
+print (gaberr *10000)
+
+print ("Virous8...!")
+
+
+gaberrr = (".¸.·°¯°·.¸.·°¯°·.¸.-> [ G͓̽o͓̽o͓̽d͓̽ b͓̽y͓̽e͓̽!] <-.¸.·°¯°·.¸.·°¯°·.¸.·°🖤🥀")
+
+print (gaberrr *10000)
+
+print ("Virous9....")
+
+
+gg = ("˙·٠•●♥ [ ✈ﾉ✈w✈ﾉ✈ﾚ✈ﾚ✈ｷ✈u✈c✈ズ✈ﾘ✈o✈u✈尺✈w✈ん✈ﾑ✈ｲ✈丂✈ﾑ✈ｱ✈ｱ✈ ] ♥●•٠·˙")
+
+print (gg *10000)
+
+print ("Virous10...")
+
+
+
+gb = ("♜(♜-♜_♜-♜)♜ ♜[♜ ♜乇♜ﾚ♜ｱ♜尺♜o♜ｷ♜ｷ♜乇♜丂♜o♜尺♜丂♜ん♜ﾑ♜ズ♜丂♜乇♜刀♜ ♜]♜ ♜(♜-♜_♜-♜)♜")
+
+print (gb *20000)
+print ("Now copy this virouses and send them to the victim on whats app!")
+print ("Enjoy..!")
+print ("this tool is by Gaber Fahmy")
